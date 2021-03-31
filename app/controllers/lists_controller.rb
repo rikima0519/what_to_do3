cb463cff038c183e30ba6c4b5d682c9e350c62ae
rikima0519 <s.rikima@icloud.com>
2021-03-31 17:11:46 +0900

    リスト作成機能実装

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,25 @@
 class ListsController < ApplicationController
 
   def index
-
+    @lists = current_user.lists.includes(:user).order('day ASC')
   end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to :root
+    else
+      render action: :new
+    end
+  end
+
+  private
+    def list_params
+      params.require(:list).permit(:title).merge(user_id: current_user.id)
+    end
 
 end
