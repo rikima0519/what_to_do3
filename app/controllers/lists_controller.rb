772dd1a5aea8a13cde4b9d0945ecc5ba6b900404
rikima0519 <s.rikima@icloud.com>
2021-03-31 17:20:22 +0900

    リスト編集、削除機能実装

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,6 +17,25 @@ class ListsController < ApplicationController
     end
   end
 
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    if list.update(list_params)
+      redirect_to :root
+    else
+      render 'edit'
+     end
+  end
+
+  def destroy
+     list = List.find(params[:id])
+     list.destroy
+     redirect_to :root
+  end
+
   private
     def list_params
       params.require(:list).permit(:title).merge(user_id: current_user.id)
